649c2f35d9f0afb0d17b354021019bd4b81014f7
jxmot <webdev@stupiddog.net>
2024-12-20 08:24:00 -0600

    initial commit, bash script for downloading images from picsum.photos

---
+++ assets/img/grabimg.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+curl -L https://picsum.photos/300 -o img-1.jpg
+curl -L https://picsum.photos/300 -o img-2.jpg
+curl -L https://picsum.photos/300 -o img-3.jpg
+curl -L https://picsum.photos/300 -o img-4.jpg
+curl -L https://picsum.photos/300 -o img-5.jpg
+curl -L https://picsum.photos/300 -o img-6.jpg
+curl -L https://picsum.photos/300 -o img-7.jpg
+curl -L https://picsum.photos/300 -o img-8.jpg
+curl -L https://picsum.photos/300 -o img-9.jpg
+curl -L https://picsum.photos/300 -o img-10.jpg
+curl -L https://picsum.photos/300 -o img-11.jpg
+curl -L https://picsum.photos/300 -o img-12.jpg
+curl -L https://picsum.photos/300 -o img-13.jpg
+curl -L https://picsum.photos/300 -o img-14.jpg
+curl -L https://picsum.photos/300 -o img-15.jpg
+curl -L https://picsum.photos/300 -o img-16.jpg
+curl -L https://picsum.photos/300 -o img-17.jpg
+curl -L https://picsum.photos/300 -o img-18.jpg
+curl -L https://picsum.photos/300 -o img-19.jpg
+curl -L https://picsum.photos/300 -o img-20.jpg
